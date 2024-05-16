dcc22ac6566b5acdb62cdf4974139ddb5ec82ae2
LesPaul997 <mariogiordano941@gmail.com>
2024-05-16 22:30:38 +0200

    add multiples and disabled

--- js/main.js
+++ js/main.js
@@ -6,16 +6,18 @@
 
 for (let i = 1; i <= 100; i++) {
     //Qui stampo i numeri da 1 a 100
-    console.log(i);
+    /* console.log(i); */
 
-        if (i%3==0) {
+    //Qui stampo solo i multipli di 3 e 5
+        if (i%15==0) {
+            console.log('FizzBuzz');
+    //Da qui in poi gestisco i divisibili
+        } else if (i%3==0) {
             console.log('fizz');
         } else if (i%5==0) {
             console.log('buzz');
-        } else if (i%3==0)(i%5==0)
-            console.log('FizzBuzz');
-        
-
-
+        }  else {
+            console.log(i);
+        }
 
 }
