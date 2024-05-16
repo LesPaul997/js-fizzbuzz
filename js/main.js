c2fc95dd178422914193d67177ea9f2a8f2ec168
LesPaul997 <mariogiordano941@gmail.com>
2024-05-16 22:18:49 +0200

    add : if content

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+//Scrivi un programma che stampi in console i numeri da 1 a 100
+
+
+for (let i = 1; i <= 100; i++) {
+    //Qui stampo i numeri da 1 a 100
+    console.log(i);
+
+        if (i%3==0) {
+            console.log('fizz');
+        } else if (i%5==0) {
+            console.log('buzz');
+        } else if (i%3==0)(i%5==0)
+            console.log('FizzBuzz');
+        
+
+
+
+}
